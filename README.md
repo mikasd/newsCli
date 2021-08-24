a58ef60627bd8dd3cf878761e6dad82185b85790
mingley <54518@icf.com>
2021-08-23 17:08:47 -0700

    finishing touches

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # newsCli
-view news from the terminal
+view news from the terminal. Just build and run it. Top currently hardcoded for top us news. 
+
+![screenshot](/img/screenshot.png)
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
 use std::error::Error;
 
+use colour::{dark_cyan_ln, dark_magenta_ln};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
 struct Articles {
-    articles: Vec<Article>
+    articles: Vec<Article>,
 }
 
+#[derive(Deserialize, Debug)]
 struct Article {
     title: String,
-    url: String
+    url: String,
 }
 
-fn get_articles(url: &str) -> Result<Article, Box<dyn Error>> {
+fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
     let res = ureq::get(url).call()?.into_string()?;
 
-    dbg!(res);
+    let articles: Articles = serde_json::from_str(&res)?;
 
-    todo!()
+    Ok(articles)
 }
 
-fn main() {
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=be85b8f066ce4f16a754cb1cc91f7a60";
-    let articles = get_articles(url);
-    println!("Hello, world!");
+fn display_articles(fetched_articles: &Articles) {
+    for article in &fetched_articles.articles {
+        dark_cyan_ln!("> {}", article.title);
+        dark_magenta_ln!("== {}\n", article.url);
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=GET_YOUR_OWN_API_KEY_LOL";
+    let articles = get_articles(url)?;
+
+    display_articles(&articles);
+
+    Ok(())
 }
